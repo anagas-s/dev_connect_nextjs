238bba012c143cdef1752a2333b0c017fa6a805c
Anagas <anagas.s777@gmail.com>
2024-06-17 09:09:41 +0530

    add username

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -59,14 +59,13 @@ export async function POST(req: Request) {
   console.log({ eventType });
   console.log({ evt });
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, username, first_name, last_name } =
-      evt.data;
+    const { id, email_addresses, image_url, first_name, last_name } = evt.data;
 
     // Create a new user in the database
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name} ${last_name ? ` ${last_name}` : ""}`,
-      username: username!,
+      username: " username!",
       email: email_addresses[0].email_address,
       picture: image_url,
     });
